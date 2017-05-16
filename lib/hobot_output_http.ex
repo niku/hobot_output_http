1144d32febe8635b2cc341f6bb5082285d496615
niku <niku@niku.name>
2017-05-17 01:00:41 +0900

    Take plugin_options as arguments of start_link

--- lib/hobot_output_http.ex
+++ lib/hobot_output_http.ex
@@ -17,31 +17,47 @@ defmodule Hobot.Output.HTTP do
     end
   end
 
-  def start_link(args, options \\ [])
-  def start_link(topic_map, options) when is_map(topic_map) do
-    GenServer.start_link(__MODULE__, topic_map, options)
+  def pass_through_filters(data, filters) when is_list(filters) do
+    Enum.reduce(filters, data, &(apply(&1, [&2])))
   end
 
-  def init(topic_map) do
+  def put_into_arguments(data, arguments) do
+    put_in(arguments, [Access.at(1), Access.elem(3)], data)
+  end
+
+  def do_http_request(arguments) do
+    apply(:httpc, :request, arguments)
+  end
+
+  def start_link(topic_map, plugin_options \\ [], genserver_options \\ [])
+  def start_link(topic_map, plugin_options, genserver_options) when is_map(topic_map) do
+    GenServer.start_link(__MODULE__, {topic_map, plugin_options}, genserver_options)
+  end
+
+  def init({topic_map, plugin_options}) do
     for {topic, _} <- topic_map, do: Hobot.subscribe(topic)
-    {:ok, topic_map}
+    {:ok, {topic_map, plugin_options}}
   end
 
-  def handle_cast({:broadcast, topic, data}, topic_map) do
+  def handle_cast({:broadcast, topic, data}, {topic_map, plugin_options}) do
     case match(topic_map, topic) do
-      {:ok, value} ->
-        body_putted = put_in(value, [Access.at(1), Access.elem(3)], data)
-        result = apply(:httpc, :request, body_putted)
+      {:ok, arguments_for_httpc_request} ->
+        filters = Keyword.get(plugin_options, :filters, [])
+        result =
+          data
+          |> pass_through_filters(filters)
+          |> put_into_arguments(arguments_for_httpc_request)
+          |> do_http_request
         Logger.debug inspect(result)
       {:error, :no_match} ->
         Logger.warn "no maching data found"
       {:error, unexpected_data_structure} ->
         Logger.warn "unexpected data strcture given: #{inspect unexpected_data_structure}"
     end
-    {:noreply, topic_map}
+    {:noreply, {topic_map, plugin_options}}
   end
 
-  def terminate(reason, topic_map) do
+  def terminate(reason, {topic_map, _plugin_options}) do
     for {topic, _} <- topic_map, do: Hobot.unsubscribe(topic)
     reason
   end
